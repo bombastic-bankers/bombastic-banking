49ad916f18639f6c40c3da994e7c4a9ed868291e
Atifah28 <S1058932@connect.np.edu.sg>
2025-11-16 18:10:32 +0800

    added deposit processing,withdraw & deposit completion pages

---
+++ atm/src/routes/withdraw-completion/+page.svelte
@@ -0,0 +1,52 @@
+<div class="w-screen h-screen bg-[#111a27] flex items-center justify-center">
+  <div class="bg-[#0c1525] shadow-xl rounded-3xl border border-gray-700
+              w-[90%] max-w-[900px] h-[80%]
+              flex flex-col items-center relative">
+
+    <!-- TOP BAR -->
+    <div class="w-full h-20 bg-red-600 rounded-t-3xl flex items-center justify-center
+                text-white text-3xl font-semibold tracking-wide">
+      OCBC
+    </div>
+
+    <!-- MAIN -->
+    <div class="flex-1 w-full flex flex-col items-center px-8 py-10 text-center">
+
+           <!-- SUCCESS ICON -->
+      <div class="w-18 h-18 rounded-full border-4 border-green-500 flex items-center justify-center">
+        <div class="w-6 h-12 border-b-4 border-r-4 border-green-500 rotate-45"></div>
+      </div>
+
+
+      <h2 class="text-white text-2xl font-semibold mt-6">Withdraw Complete!</h2>
+      <p class="text-gray-400 mt-1">Thank you for using OCBC Bank ATM</p>
+
+      <button class="mt-10 px-8 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg text-lg">
+        New Transaction
+      </button>
+
+      <button class="mt-4 px-8 py-3 bg-white text-red-500 border border-red-300 rounded-lg text-lg">
+        Take Out Card & Exit
+      </button>
+    </div>
+
+    <!-- BOTTOM VENTS & SLOT -->
+    <div class="absolute bottom-6 w-full flex flex-col items-center">
+      <div class="flex justify-between w-[60%] text-gray-600 mb-3">
+        <div class="space-y-1">
+          <div class="h-1 w-8 bg-gray-700 rounded"></div>
+          <div class="h-1 w-8 bg-gray-700 rounded"></div>
+          <div class="h-1 w-8 bg-gray-700 rounded"></div>
+        </div>
+        <div class="space-y-1">
+          <div class="h-1 w-8 bg-gray-700 rounded"></div>
+          <div class="h-1 w-8 bg-gray-700 rounded"></div>
+          <div class="h-1 w-8 bg-gray-700 rounded"></div>
+        </div>
+      </div>
+
+      <div class="h-5 w-64 bg-black rounded-lg shadow-inner"></div>
+    </div>
+
+  </div>
+</div>
