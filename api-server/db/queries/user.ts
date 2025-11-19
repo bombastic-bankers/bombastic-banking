1d295ec7832d817d49e786d551a365c152910163
Haziq Hairil <haziqhairil21@gmail.com>
2025-11-19 11:39:08 +0800

    GET user info

--- api-server/db/queries/user.ts
+++ api-server/db/queries/user.ts
@@ -1,6 +1,6 @@
 import { db } from "../index.js";
-import { users } from "../schema.js";
-import { eq } from "drizzle-orm";
+import { ledger, users } from "../schema.js";
+import { eq, sum } from "drizzle-orm";
 
 export async function createUser(
   user: typeof users.$inferInsert,
@@ -14,4 +14,23 @@ export async function getUserByEmail(
   return (
     (await db.select().from(users).where(eq(users.email, email))).at(0) ?? null
   );
+}
+
+export async function getUserInfo(
+  userId: number,
+): Promise<{ fullName: string; accountBalance: number }> {
+  const { fullName } = (
+    await db
+      .select({ fullName: users.fullName })
+      .from(users)
+      .where(eq(users.userId, userId))
+  )[0];
+  const { accountBalance: accountBalanceString } = (
+    await db
+      .select({ accountBalance: sum(ledger.amount) })
+      .from(ledger)
+      .where(eq(ledger.userId, userId))
+  )[0];
+
+  return { fullName, accountBalance: +(accountBalanceString ?? 0) };
 }
