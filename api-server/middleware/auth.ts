61f9e3a0b52773f34d57f71b7d9f3561348eed4d
Haziq Hairil <haziqhairil21@gmail.com>
2025-11-23 00:39:38 +0800

    Remove debug logging

--- api-server/middleware/auth.ts
+++ api-server/middleware/auth.ts
@@ -3,8 +3,6 @@ import { Request, Response, NextFunction } from "express";
 import { JWT_SECRET } from "../env.js";
 
 export function authenticate(req: Request, res: Response, next: NextFunction) {
-  console.log(`general auth hit`);
-
   const auth = req.headers.authorization;
 
   if (!auth || !auth.startsWith("Bearer ")) {
