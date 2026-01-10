c3d0e3395d7ead3ecf68ae79a7a91c02ff05af2e
Haziq Hairil <haziqhairil21@gmail.com>
2026-01-10 15:58:13 +0800

    Base64URL instead of hex for refresh tokens
    More compact

--- api-server/services/auth.ts
+++ api-server/services/auth.ts
@@ -8,7 +8,7 @@ import { JWT_SECRET } from "../env";
 export async function generateAuthTokens(userId: number) {
   const accessToken = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "2m" });
 
-  const refreshToken = crypto.randomBytes(40).toString("hex");
+  const refreshToken = crypto.randomBytes(40).toString("base64url");
   const expiresAt = new Date();
   expiresAt.setDate(expiresAt.getDate() + 30);
 
