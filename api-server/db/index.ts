689d17888fc3a9808294e62eb0ba3eeaa374edf2
Haziq Hairil <haziqhairil21@gmail.com>
2026-01-12 09:50:04 +0800

    Lazily load db conn

--- api-server/db/index.ts
+++ api-server/db/index.ts
@@ -1,4 +1,14 @@
 import { drizzle } from "drizzle-orm/neon-serverless";
 import env from "../env.js";
 
-export const db = drizzle(env.DATABASE_URL);
+let _db: ReturnType<typeof drizzle> | null = null;
+
+/**
+ * Lazily-initialized database connection.
+ */
+export const db = new Proxy({} as ReturnType<typeof drizzle>, {
+  get(_, prop) {
+    if (!_db) _db = drizzle(env.DATABASE_URL);
+    return Reflect.get(_db, prop);
+  },
+});
