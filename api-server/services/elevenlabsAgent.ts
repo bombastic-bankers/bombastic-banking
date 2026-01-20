716801efb0b8f6a19db61285395fddbfdff85ff4
YAO1267 <S10268880@connect.np.edu.sg>
2026-01-20 08:38:04 +0800

    solve failing unit test

--- api-server/services/elevenlabsAgent.ts
+++ api-server/services/elevenlabsAgent.ts
@@ -3,14 +3,6 @@ import { ElevenLabsClient } from "@elevenlabs/elevenlabs-js";
 const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY!;
 const ELEVENLABS_AGENT_ID = process.env.ELEVENLABS_AGENT_ID!;
 
-if (!ELEVENLABS_API_KEY) {
-  throw new Error("ELEVENLABS_API_KEY is not set in environment");
-}
-
-if (!ELEVENLABS_AGENT_ID) {
-  throw new Error("ELEVENLABS_AGENT_ID is not set in environment");
-}
-
 const client = new ElevenLabsClient({
   apiKey: ELEVENLABS_API_KEY,
   environment: "https://api.elevenlabs.io",
